a1c6c240bfb5a8aa394b41bed7879a5bb90a6b72
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-17 11:56:41 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = (props) => {
+  const [flag, setFlag] = useState(false);
+
+  function handleClick() {
+    setFlag(true);
+  }
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      {flag && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      <button id="click" onClick={handleClick}>Click Me</button>
     </div>
-  );
-}
+  ); // prettier-ignore
+};
 
-export default App
+export default App;
